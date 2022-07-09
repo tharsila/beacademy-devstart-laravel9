e7c132a29f69aecba1ca2a81577200ba77c7d79a
tharsila <tharsilapaula@gmail.com>
2022-07-08 22:50:01 -0300

    add metodo show

--- meu-app-laravel/app/Http/Controllers/UserController.php
+++ meu-app-laravel/app/Http/Controllers/UserController.php
@@ -2,21 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index() 
     {
-        $users = [
-            'nomes' => ['Gatuni', 'Yoshi'] 
-        ];
-
-        dd($users);
+        $users = User::all();
+        
+        return view ('users.index', compact('users'));
     }
 
     public function show($id)
     {
-        dd('Id do usuário é: ' . $id);
+       /*  $user = User::where('id', $id)->first(); */
+        if(!$user = User::find($id)) {
+            return redirect()->route('users.index');
+        }
+
+        return view('users.show', compact('user'));
     }
 }
