54589f03aec48725658d3d474c0bc9ff0154632f
tharsila <tharsilapaula@gmail.com>
2022-07-14 23:47:13 -0300

    add construct and crud for users

--- meu-app-laravel/app/Http/Controllers/UserController.php
+++ meu-app-laravel/app/Http/Controllers/UserController.php
@@ -4,14 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreUpdateUserFormRequest;
 
 class UserController extends Controller
 {
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+
     public function index() 
     {
         $users = User::all();
         
         return view ('users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view ('users.create');
+    }
+
+    public function store(StoreUpdateUserFormRequest $request)
+    {
+        $data = $request->all();
+        $data['password'] = bcrypt($data['password']);
+
+        $this->model->create($data);
+
+        return redirect()->route('users.index');
     }
 
     public function show($id)
@@ -21,6 +42,41 @@ class UserController extends Controller
             return redirect()->route('users.index');
         }
 
-        return view('users.show', compact('user'));
+        $title = "Usuário {$user->name}";
+
+        return view('users.show', compact('user', 'title'));
+    }
+
+    public function edit($id)
+    {
+        $user = User::find($id);
+        if(!$user) {
+            return redirect()->route('users.index');
+        }
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(StoreUpdateUserFormRequest $request, $id)
+    {
+        $user = User::find($id);
+        if(!$user) {
+            return redirect()->route('users.index');
+        } else {
+            $data = $request->only('name', 'email');
+            if ($request->password) {
+                $data['password'] = bcrypt($request->password);  
+            }
+            $user->update($data);
+            return redirect()->route('users.index');
+        }
+    }
+
+    public function destroy($id) {
+        $user = User::find($id);
+        if(!$user) {
+            return redirect()->route('users.index');
+        }
+        $user->delete();
+        return redirect()->route('users.index');
     }
 }
