2695a672e0015501c3dc351ff68d5a3388a54c36
tharsila <tharsilapaula@gmail.com>
2022-07-14 23:43:46 -0300

    add addresses

--- meu-app-laravel/routes/web.php
+++ meu-app-laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{UserController, ViaCepController};
+use App\Http\Controllers\{UserController, ViaCepController, AddressController};
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +19,14 @@ Route::get('/', function () {
 });
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+
+Route::get('/addresses', [AddressController::class, 'index'])->name('address.index');
 
 Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
 Route::post('/viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
