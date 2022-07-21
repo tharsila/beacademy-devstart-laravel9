abf837c42021d562784044630dbfbfcb1a7f6fd5
tharsila <tharsilapaula@gmail.com>
2022-07-21 18:53:35 -0300

    visualiza quantidade de posts do usuario

--- meu-app-laravel/resources/views/template/users.blade.php
+++ meu-app-laravel/resources/views/template/users.blade.php
@@ -9,7 +9,19 @@
   <title>@yield('title')</title>
 </head>
 <body>
-  <div class="container">
+  <div class="container w-75 p-3">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link text-white" href="/users">Usuários</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="/posts">Post</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     @yield('content')
   </div>
 </body>
