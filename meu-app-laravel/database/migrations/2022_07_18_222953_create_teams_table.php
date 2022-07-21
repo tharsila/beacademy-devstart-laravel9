ea43a37a792dcd5be45dc9cbab69246fdc16a6cf
tharsila <tharsilapaula@gmail.com>
2022-07-21 18:54:56 -0300

    migration para users, posts e teams

---
+++ meu-app-laravel/database/migrations/2022_07_18_222953_create_teams_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('team_user', function (Blueprint $table) {
+            $table->id();
+            /* $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('team_id');
+            $table->unique('team_id')->references('id')->on('teams'); */
+            $table->foreignId('user_id')
+            ->constrained('users')
+            ->onUpdate('CASCADE')
+            ->onDelete('CASCADE');
+            $table->foreignId('team_id')
+            ->constrained('teams')
+            ->onUpdate('CASCADE')
+            ->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+        Schema::dropIfExists('team_user');
+    }
+};
